6de038a4b112ea6e7c6ee0c1c4b806871f772371
Sergey Anufriev <anufriiev.sergii@gmail.com>
2021-11-12 11:25:43 +0000

    added string module, new function added to nums

--- first/nums.py
+++ first/nums.py
@@ -6,9 +6,13 @@ text = 'hello world'
 def mult(x,y):
     return x*y
 
+
+def say_hello():
+    return 'hello world'
+
 if __name__ == '__main__':
     print(sum(2,3))
     print(mult(4,6))
-
+    print(say_hello())
 
 
---
+++ first/strings.py
@@ -0,0 +1,9 @@
+def bark():
+    return 'gav,gav!!!'
+
+
+
+if __name__ == '__main__':
+    bark()
+
+
